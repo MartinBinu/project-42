187414d7342ac6ba9a2774c8d1b173f135a9b887
MartinBinu <68335599+MartinBinu@users.noreply.github.com>
2021-05-04 14:33:30 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -19,15 +19,16 @@ function setup() {
 function draw() {
   background("black");  
    translate(200,200);
+   rotate(-90)
    angleMode(DEGREES);
 
    hr = hour();
    min = minute();
    sec = second();
 
-   hrAngle = map(hr%12 - 3,0,12,0,360);
-   minAngle = map(min - 15,0,60,0,360);
-   secAngle = map(sec - 15,0,60,0,360);
+   hrAngle = map(hr%12,0,12,0,360);
+   minAngle = map(min,0,60,0,360);
+   secAngle = map(sec,0,60,0,360);
 
    push();
    rotate(hrAngle);
@@ -49,20 +50,26 @@ function draw() {
    strokeWeight(7);
    line(0,0,100,0);
    pop();
-
+ 
    fill("black");
    dark = ellipse(1,10,100,100);
 
+   rotate(90);
    textSize(20);
    fill("red");
-   text(hr%12 + ":",-35,19)
+   text(hr%12 + ":",-34,10)
    fill(0,255,0);
-   text(min + ":",-20,19);
+   text(min + ":",-9,10);
    fill("blue");
-   text(sec,7,19)
+   text(sec,28,10)
 
+   rotate(-90);
    noFill();
-   arc(-20,19,200,200,0 - 90,hrAngle);
-   arc(-20,19,300,300,0 - 90,minAngle);
-   arc(-20,19,200,200,0 - 90,secAngle);
+   stroke("red");
+   strokeWeight(10)
+   arc(0,0,300,300,0,hrAngle);
+   stroke(0,255,0);
+   arc(0,0,325,325,0,minAngle);
+   stroke("blue");
+   arc(0,0,350,350,0,secAngle);
 }
